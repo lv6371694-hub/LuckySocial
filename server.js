cb8a542e1bcbd677525d2aec10b7276a49355d93
lv6371694-hub <lv6371694@gmail.com>
2026-07-29 21:38:47 +0530

    Use bcryptjs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();

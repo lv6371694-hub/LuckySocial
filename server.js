ae00514d6578687d32fdbc14e52714ca728cd1d6
lv6371694-hub <lv6371694@gmail.com>
2026-07-29 18:21:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,31 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Static files serve karo
+// Static files
 app.use(express.static(__dirname));
+app.use(express.json());
 
 // Home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Dummy API (Feed)
+app.get("/api/posts", (req, res) => {
+  res.json([
+    {
+      user: "Lucky",
+      text: "Welcome to LuckySocial 🚀",
+      image: "",
+      likes: 120
+    },
+    {
+      user: "Admin",
+      text: "Instagram style social app is working!",
+      image: "",
+      likes: 56
+    }
+  ]);
 });
 
 app.listen(PORT, () => {
